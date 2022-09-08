43fd139f5d4ed7a3e48d57b23d05061d49afa204
jinnyzg <jingyz@bu.edu>
2022-09-08 18:13:10 -0400

    add database

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
 from flask_script import Manager
 from project.server import app
+from flask_migrate import MigrateCommand
 
 
 manager = Manager(app)
+
+# Add the flask migrate
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
